29b98558f53ac246fc0434567d523f75961c7181
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-08-03 19:25:05 -0300

    calculadora 99%

--- js/index.js
+++ js/index.js
@@ -1,25 +1,53 @@
 let prev = 0
 let current = 0
-let soma = 0
+let soma = 10
+let sub = 0
+let mult = 0
+let div = 0
+let operacao = 0
+let who;
 function limpar() {
+    previous.innerHTML = ''
     res.innerHTML = ''
     current = ''
     prev = ''
 }
 function digitarNumero(num) {
-    current += num
+    if(num == '.' && current.indexOf('.') == -1){
+        current += num
+        res.innerHTML = current
+    }else if(){
+
+    }
+}
+
+function apagar() {
+    current = current.toString().slice(0, -1)
     res.innerHTML = current
 }
-function somar() {
+function operador(e) {
+    who = e
+    transisao()
+}
+function transisao() {
     prev = current
     current = ''
-    res.innerHTML = ''
-}
-function apagar() {
-    res.innerHTML = current.length
+    res.innerHTML = current
+    previous.innerHTML = prev
 }
 function total() {
-    soma = Number(current) + Number(prev)
-    res.innerHTML = soma
+    if (who == 'soma') {
+        current = Number(prev) + Number(current)
+    } else if (who == 'sub') {
+        current = Number(prev) - Number(current)
+    } else if (who == 'mult') {
+        current = Number(prev) * Number(current)
+    } else if (who == 'div') {
+        current = Number(prev) / Number(current)
+    } else {
+        res.innerHTML = 'Sabe não ó'
+    }
+    res.innerHTML = current
+    previous.innerHTML = ''
 }
 limpar()
